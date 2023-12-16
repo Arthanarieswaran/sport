998f498920070d08601d8d0823346a386c0c6a29
Arthanarieswaran <arthanarieswaran2002sn@gmail.com>
2023-12-16 17:09:01 +0530

    kj

--- resources/views/frontend/layouts/gamelist.blade.php
+++ resources/views/frontend/layouts/gamelist.blade.php
@@ -3,93 +3,77 @@
 
 
 <div class="d-flex flex-row">
-    <div class="d-flex flex-row">
-
-    <div class="a-box">
-        <div class="img-container">
-          <div class="img-inner">
-            <div class="inner-skew">
-              <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
+    <div class="d-flex flex-row list-card" style="margin-left: 28vh">
+            <div class="d-flex flex-row mt-5">
+                <div class="a-box me-5">
+                    <div class="img-container">
+                      <div class="img-inner">
+                        <div class="inner-skew">
+                          <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
+                        </div>
+                      </div>
+                    </div>
+                    <a href="" class="text-decoration-none">
+                        <div class="text-container">
+                        <h3>A blue bird</h3>
+                        <div>
+                            This a demo experiment to skew image container. It looks good.
+                        </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="a-box me-5">
+                    <div class="img-container">
+                      <div class="img-inner">
+                        <div class="inner-skew">
+                          <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
+                        </div>
+                      </div>
+                    </div>
+                    <a href="" class="text-decoration-none">
+                        <div class="text-container">
+                        <h3>A blue bird</h3>
+                        <div>
+                            This a demo experiment to skew image container. It looks good.
+                        </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="a-box me-5">
+                    <div class="img-container">
+                      <div class="img-inner">
+                        <div class="inner-skew">
+                          <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
+                        </div>
+                      </div>
+                    </div>
+                    <a href="" class="text-decoration-none">
+                        <div class="text-container">
+                        <h3>A blue bird</h3>
+                        <div>
+                            This a demo experiment to skew image container. It looks good.
+                        </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="a-box me-5">
+                    <div class="img-container">
+                      <div class="img-inner">
+                        <div class="inner-skew">
+                          <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
+                        </div>
+                      </div>
+                    </div>
+                    <a href="" class="text-decoration-none">
+                        <div class="text-container">
+                        <h3>A blue bird</h3>
+                        <div>
+                            This a demo experiment to skew image container. It looks good.
+                        </div>
+                        </div>
+                    </a>
+                </div>
             </div>
-          </div>
-        </div>
-        <div class="text-container">
-          <h3>A blue bird</h3>
-          <div>
-            This a demo experiment to skew image container. It looks good.
-        </div>
     </div>
-    {{-- <div class="a-box">
-        <div class="img-container">
-          <div class="img-inner">
-            <div class="inner-skew">
-              <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
-            </div>
-          </div>
-        </div>
-        <div class="text-container">
-          <h3>A blue bird</h3>
-          <div>
-            This a demo experiment to skew image container. It looks good.
-        </div>
-      </div> --}}
-      {{-- <div class="a-box">
-        <div class="img-container">
-          <div class="img-inner">
-            <div class="inner-skew">
-              <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
-            </div>
-          </div>
-        </div>
-        <div class="text-container">
-          <h3>A blue bird</h3>
-          <div>
-            This a demo experiment to skew image container. It looks good.
-        </div>
-      </div> --}}
-      {{-- <div class="a-box">
-        <div class="img-container">
-          <div class="img-inner">
-            <div class="inner-skew">
-              <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
-            </div>
-          </div>
-        </div>
-        <div class="text-container">
-          <h3>A blue bird</h3>
-          <div>
-            This a demo experiment to skew image container. It looks good.
-        </div>
-      </div> --}}
-      {{-- <div class="a-box">
-        <div class="img-container">
-          <div class="img-inner">
-            <div class="inner-skew">
-              <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
-            </div>
-          </div>
-        </div>
-        <div class="text-container">
-          <h3>A blue bird</h3>
-          <div>
-            This a demo experiment to skew image container. It looks good.
-        </div>
-      </div> --}}
-      {{-- <div class="a-box">
-        <div class="img-container">
-          <div class="img-inner">
-            <div class="inner-skew">
-              <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
-            </div>
-          </div>
-        </div>
-        <div class="text-container">
-          <h3>A blue bird</h3>
-          <div>
-            This a demo experiment to skew image container. It looks good.
-        </div>
-      </div> --}}
-      </div>
 </div>
 @endsection
-
